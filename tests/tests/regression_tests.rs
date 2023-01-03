dfa4f471246e775457b0776f8e2693698eb6ffcd
MrCheeze <fishycheeze@yahoo.ca>
2023-01-03 10:56:20 -0500

    test: rewinding to a frame before a child clip was created should set the child to null

--- tests/tests/regression_tests.rs
+++ tests/tests/regression_tests.rs
@@ -345,6 +345,7 @@ swf_tests! {
     (as3_getouterscope, "avm2/getouterscope", 1),
     (as3_goto_methods, "avm2/goto_methods", 1),
     (as3_goto_methods_swfver10, "avm2/goto_methods_swfver10", 1),
+    (as3_goto_rewind_remove_child, "avm2/goto_rewind_remove_child", 1),
     (as3_greaterequals, "avm2/greaterequals", 1),
     (as3_greaterthan, "avm2/greaterthan", 1),
     (as3_has_own_property, "avm2/has_own_property", 1),
